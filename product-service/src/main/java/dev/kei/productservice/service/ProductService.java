3cc57941fe07fec16b89057d4a484d15e1c9f0e7
Kei-K23 <keiksl2301@gmail.com>
2024-05-06 21:30:53 +0630

    Add:product-service create product and get all product services

--- product-service/src/main/java/dev/kei/productservice/service/ProductService.java
+++ product-service/src/main/java/dev/kei/productservice/service/ProductService.java
@@ -1,9 +1,12 @@
 package dev.kei.productservice.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.kei.productservice.dto.ProductRequest;
+import dev.kei.productservice.dto.ProductResponse;
 import dev.kei.productservice.model.Product;
 import dev.kei.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +27,15 @@ public class ProductService {
                 .build();
         productRepository.save(product);
         log.info("Product {} is saved.", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder().id(product.getId()).name(product.getName())
+                .description(product.getDescription()).price(product.getPrice()).build();
     }
 }
